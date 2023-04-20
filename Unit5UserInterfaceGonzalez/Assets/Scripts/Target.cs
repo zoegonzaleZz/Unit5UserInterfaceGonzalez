a5ff724e37a4d78468442d16ea6bd0830e5c58b3
zoegonzaleZz <117196164+zoegonzaleZz@users.noreply.github.com>
2023-04-20 09:42:40 -0700

    done with 5.1
    on 5.2 part4

--- Unit5UserInterfaceGonzalez/Assets/Scripts/Target.cs
+++ Unit5UserInterfaceGonzalez/Assets/Scripts/Target.cs
@@ -9,7 +9,7 @@ public class Target : MonoBehaviour
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
-    private float ySpawnPos = -6;
+    private float ySpawnPos = -3;
 
     // Start is called before the first frame update
     void Start()
@@ -20,7 +20,20 @@ public class Target : MonoBehaviour
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
         transform.position = RandomSpawnPos();
     }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
     Vector3 RandomForce()
     {
         return Vector3.up * Random.Range(minSpeed, maxSpeed);
@@ -32,11 +45,5 @@ public class Target : MonoBehaviour
     Vector3 RandomSpawnPos()
     {
         return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
